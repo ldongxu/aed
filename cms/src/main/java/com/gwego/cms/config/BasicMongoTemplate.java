9071665ca7d7a38eac90966bc08ee8518a5af05a
liudongxu06 <liudongxu06@58ganji.com>
2018-06-19 20:33:20 +0800

    mongo数据库

---
+++ cms/src/main/java/com/gwego/cms/config/BasicMongoTemplate.java
@@ -0,0 +1,37 @@
+package com.gwego.cms.config;
+
+import com.mongodb.MongoClientURI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+/**
+ * @author liudongxu06
+ * @date 2018/6/19
+ */
+@Configuration
+@EnableMongoRepositories(basePackages = "com.gwego.cms.repository",mongoTemplateRef = "basicMongo")
+public class BasicMongoTemplate {
+    @Autowired
+    @Qualifier("basicMongoProperties")
+    private MongoProperties mongoProperties;
+
+    @Primary
+    @Bean
+    public MongoDbFactory basicFactory(MongoProperties mongoProperties){
+        return new SimpleMongoDbFactory(new MongoClientURI(mongoProperties.getUri()));
+    }
+
+    @Primary
+    @Bean(name = "basicMongo")
+    public MongoTemplate basicMongoTemplate(){
+        return new MongoTemplate(basicFactory(this.mongoProperties));
+    }
+}
